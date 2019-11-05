a2359adac5460297374835aed529e36daab58ac6
natsuya yamashiro <azuki728@me.com>
2019-11-05 19:16:53 +0900

    Initial Commit

---
+++ FirstKadaiApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp2
+//
+//  Created by NATSUYA YAMASHIRO on 2019/11/05.
+//  Copyright © 2019 natsuya.yamashiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
